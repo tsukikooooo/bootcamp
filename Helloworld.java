39dc177bb463d1a3a309606d4f592849e3a2694f
tsukikooooo <moonpun2323@gmail.com>
2024-05-08 21:55:56 +0800

    update

---
+++ Helloworld.java
@@ -0,0 +1,17 @@
+public class Helloworld{
+  //Pressthe "Run" button to execute the program
+  public static void main(String[] args ) {
+    //testing // can typing notes
+
+    // Tool:  print out something...
+    //System.out.println()
+    System.out.println("hello !!");
+    System.out.println("Welcome to bootcamp!!");
+    //End of the program
+
+  
+
+  
+  }
+}
+
---
+++ demo-datatype/src/DemoString.java
@@ -0,0 +1,23 @@
+public class DemoString {
+  public static void main(String[] args){
+    //Sting stores text
+    String x = "hello";
+    String y = "@#@$&)@";
+    String dollar = "$10";
+    
+    String WithSpace ="....tets...e!";
+    System.out.println(WithSpace);
+
+    // + only + if string co
+    String z = x + dollar;
+    System.out.println(z); //hello$10
+
+    //remindar 
+   int remaindar = 10 % 3; //餘數 整數GE 餘數係1!!!!
+   int divide = 10 / 3; //3
+   System.out.println(remaindar); //1
+   System.out.println(divide); //3
+
+  }
+  
+}
